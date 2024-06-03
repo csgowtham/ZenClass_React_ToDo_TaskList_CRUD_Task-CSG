d494359ed8ed06c3e4539a2f02d9241c0433991f
Gowtham C S <117142879+csgowtham@users.noreply.github.com>
2024-06-03 13:58:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import EditTask from './EditTask';
+import { TaskProvider } from './TaskContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
+  <Router>
+    <TaskProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/edit/:id" element={<EditTask />} />
+      </Routes>
+    </TaskProvider>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
